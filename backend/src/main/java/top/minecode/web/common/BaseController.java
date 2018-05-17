93ccfbde0f45f087f29730182a83f162e72bd492
Bbida <284983325@qq.com>
2018-05-17 23:26:32 +0800

    Add Base Controller

--- backend/src/main/java/top/minecode/web/common/BaseController.java
+++ backend/src/main/java/top/minecode/web/common/BaseController.java
@@ -7,4 +7,15 @@ package top.minecode.web.common;
  * @author iznauy
  */
 public abstract class BaseController {
+    private static final int EXPIRE_TIME = 30;
+
+    protected void addUser(String userEmail) {
+        // Generate token
+        String token = ""; // TODO: 2018/5/17 generate token here
+        ActiveUsers.addUser(userEmail, token);
+    }
+
+    protected String getUserEmail(String token) {
+        return ActiveUsers.getUser(token);
+    }
 }
