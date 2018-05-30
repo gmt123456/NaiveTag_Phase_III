06137f85fe0cee400b7e72d33b9665dcb13dc161
Jar <924342513@qq.com>
2018-05-30 21:46:04 +0800

    add jump to home on logo

--- front/src/components/common/Logo.vue
+++ front/src/components/common/Logo.vue
@@ -1,10 +1,17 @@
 <template>
+  <div @click="goToHome">
     <span class="logo">NaiveTag</span>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "Logo"
+        name: "Logo",
+      methods:{
+          goToHome:function () {
+            this.$router.push("/");
+          }
+      }
     }
 </script>
 
@@ -13,5 +20,7 @@
     color:#1B9CFC;
     font-size: 24px;
     margin: 5px 0px 0px ;
+    cursor:pointer;
   }
+
 </style>
