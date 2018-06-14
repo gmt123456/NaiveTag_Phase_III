ef1e2e286a08c7927a09fe7adc836041e18a92a3
liao <284983325@qq.com>
2018-06-14 20:26:01 +0800

    Fix a bug that can't show requester who didn't create a task before

--- backend/src/main/java/top/minecode/dao/admin/AdministrateUserDaoImpl.java
+++ backend/src/main/java/top/minecode/dao/admin/AdministrateUserDaoImpl.java
@@ -5,15 +5,20 @@ import org.springframework.stereotype.Repository;
 import top.minecode.dao.utils.CommonOperation;
 import top.minecode.domain.admin.RequesterItem;
 import top.minecode.domain.admin.WorkerItem;
+import top.minecode.domain.user.requester.Requester;
 import top.minecode.po.requester.RequesterPO;
 import top.minecode.po.worker.WorkerPO;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created on 2018/6/2.
  * Description:
+ *
  * @author Liao
  */
 @Repository("administrateUserDaoImpl")
@@ -92,8 +97,16 @@ public class AdministrateUserDaoImpl implements AdministrateUserDao {
                 " from RequesterPO r, TaskPO t where t.ownerEmail=r.email " +
                 " group by r.email order by count(t) desc";
 
+        // Append the requester who haven't created a task
+        String noTaskRequesterHql = "select new top.minecode.domain.admin.RequesterItem(r, 0) " +
+                " from RequesterPO r where not exists(from TaskPO t where t.ownerEmail=" +
+                "r.email)";
+
         //noinspection unchecked
-        return (List<RequesterItem>) getPage(page, pageSize, hql);
+        List<RequesterItem> result = (List<RequesterItem>) getPage(page, pageSize, hql);
+        addNoTaskRequester(result, page, pageSize, getResultSize(hql), noTaskRequesterHql);
+
+        return result;
     }
 
     @Override
@@ -107,12 +120,40 @@ public class AdministrateUserDaoImpl implements AdministrateUserDao {
 
     @Override
     public List<RequesterItem> searchRequester(String key, int page, int pageSize) {
-        String hql = "select new top.minecode.domain.admin.RequesterItem(r, cast(count(t) as int)) " +
-                " from RequesterPO r, TaskPO t where (r.email like :key or r.name like :key) and " +
-                " t.ownerEmail=r.email group by r.email";
+        String hql = "select r from RequesterPO r where r.email like :key or r.name like :key";
 
         //noinspection unchecked
-        return (List<RequesterItem>) searchPage(page, pageSize, hql, key);
+        List<RequesterPO> result = CommonOperation.template(session -> {
+            Query query = session.createQuery(hql);
+            query.setString("key", "%" + key + "%");
+            //noinspection unchecked
+            return (List<RequesterPO>) query.list();
+        });
+
+        List<RequesterItem> items = countTasks(result);
+        items.sort(Comparator.comparing(RequesterItem::getTasksNum).reversed());
+
+        int start = (page - 1) * pageSize;
+        if (start >= items.size())
+            return Collections.emptyList();
+        int end = Math.min(start + pageSize, items.size());
+
+        return items.subList(start, end);
+    }
+
+    private List<RequesterItem> countTasks(List<RequesterPO> requesterPOS) {
+        String hql = "select cast(count(t) as int) from TaskPO t where t.ownerEmail=:mail";
+        List<RequesterItem> result = new ArrayList<>();
+        for (RequesterPO requesterPO : requesterPOS) {
+            String mail = requesterPO.getEmail();
+            CommonOperation.template(session -> {
+                int count = (int) session.createQuery(hql)
+                        .setParameter("mail", mail).iterate().next();
+                result.add(new RequesterItem(requesterPO, count));
+            });
+        }
+
+        return result;
     }
 
     private List searchPage(int page, int pageSize, String hql, String key) {
@@ -139,6 +180,21 @@ public class AdministrateUserDaoImpl implements AdministrateUserDao {
             query.setMaxResults(end);
             //noinspection unchecked
             return query.list();
+        });
+    }
+
+    private void addNoTaskRequester(List<RequesterItem> result, int page, int pageSize, int otherSize, String hql) {
+        //noinspection unchecked
+        if (result.size() < pageSize) {
+            int newPage = (page * pageSize - otherSize) / pageSize + 1;
+            //noinspection unchecked
+            result.addAll(getPage(newPage, pageSize - result.size(), hql));
+        }
+    }
+
+    private int getResultSize(String hql) {
+        return CommonOperation.template(session -> {
+            return session.createQuery(hql).list().size();
         });
     }
 }
